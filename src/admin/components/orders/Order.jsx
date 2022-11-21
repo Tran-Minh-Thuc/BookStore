7b8261810c3dfc0d282adb1c04f5a03440965aa1
xacnhandr001 <tranminhthuc79@gmail.com>
2022-11-21 20:46:38 +0700

    update

--- src/admin/components/orders/Order.jsx
+++ src/admin/components/orders/Order.jsx
@@ -8,41 +8,41 @@ const Order = () => {
 
   useEffect(() => {
     orderService.getAll().then((res) => {
-      console.log(res);
       setOrders(res);
     }).catch((err) => {
       console.log(err);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const OrderItem = (props) => (
-    props.data.map((item, index) => (
-        <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>
-                <img src={item.list_product[0].book.image} alt="img" />
-            </td>
-            <td>{item.list_product[0].book.name}</td>
-            <td>{item.list_product[0].book.price}</td>
-            <td>{item.address.name}</td>
-            <td>{item.address.phoneNumber}</td>
-            <td>
+  const OrderItem = (props) => {
+
+    return (props.data.map((item) => (
+      <tr key={item.id}>
+        <td>{item.id}</td>
+        <td>
+          <img src={item.list_product[0].book.image} alt="img" />
+        </td>
+        <td>{item.list_product[0].book.name}</td>
+        <td>{item.list_product[0].book.price}</td>
+        <td>{item.address.name}</td>
+        <td>{item.address.phoneNumber}</td>
+        <td>
           <span className="pr-2 hover:text-green-600 cursor-pointer">
             <i className="pi pi-info-circle" />
           </span>
 
           <span
             className="hover:text-red-600 cursor-pointer"
-            
+
           >
             <i className="pi pi-trash" />
           </span>
         </td>
-        </tr>
-    ))
-  )
+      </tr>
+    )))
 
-
+  }
 
   return (
     <div>
@@ -60,7 +60,7 @@ const Order = () => {
             </tr>
           </thead>
           <tbody className="h-10">
-            <OrderItem data = {orders} />
+            <OrderItem data={orders} />
           </tbody>
         </Table>
       </div>
